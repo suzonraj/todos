6b18832114171dd5d9f41cd821e70e9aeb5e6544
Almas MD. Estiak <suzonraj@ymail.com>
2020-01-21 17:19:03 +0600

    added TodoComponent.vue

---
+++ resources/js/components/TodoComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Todo Component</div>
+
+                    <div class="card-body">
+                        <input type="text" v-model="newTodo" @keyup.enter="addNewTodo"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            //console.log('Component mounted.')
+        },
+        props: {
+            route: {type: String, required: true}
+        },
+        data: function () {
+            return {
+                newTodo: '',
+                errors: [],
+            }
+        },
+        methods: {
+            addNewTodo() {
+                axios.post(this.route, {
+                    body: this.newTodo
+                })
+                    .then(response => {
+                        console.log(response);
+                    })
+                    .catch(e => {
+                        this.errors.push(e)
+                    })
+            }
+        },
+        // watch: {
+        //     new_task: {
+        //         immediate: true,
+        //         handler() {
+        //             this.addNewTask();
+        //         }
+        //     }
+        // }
+    }
+</script>
