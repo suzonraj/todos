3fafb82ef87d4e166200a0b6ef2a9f29b22a468d
Almas MD. Estiak <suzonraj@ymail.com>
2020-01-27 07:34:33 +0600

    added font awesome

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row mb-4">
         <div class="col">
-            <todo-component route="{{ url('/') }}"></todo-component>
+            <todo-component route="{{ url('/api/') }}"></todo-component>
         </div><!--col-->
     </div><!--row-->
 @endsection
